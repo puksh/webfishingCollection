1abe204bf0d26bb7b65a199a50cd61249a0d1069
Lucas <puksh@pm.me>
2024-11-14 12:37:43 +0100

    feat: Additional build flags for optimized performance

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,16 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  build: {
+    minify: "esbuild",
+    target: "esnext",
+    chunkSizeWarningLimit: 600,
+  },
+  css: {
+    preprocessorOptions: {
+      css: {
+        additionalData: `@import "./assets/main.css";`,
+      },
+    },
+  },
 });
