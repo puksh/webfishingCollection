78a8e82f0515d67300d1593c43aa5519ce45c489
Lucas <puksh@pm.me>
2024-11-14 12:28:03 +0100

    refactor: src/fonts -> public/fonts

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
-import "@/assets/fonts.css";
+import "/assets/fonts.css?url";
 
 createApp(App).mount("#app");
