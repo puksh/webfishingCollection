3d96e40808ae022fade1a295edca6dbd93584c76
Lucas <puksh@pm.me>
2024-11-18 00:00:20 +0100

    feat: Added navigation for Cosmetic tab

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import FishChecklist from "../components/fishChecklist.vue";
+import Cosmetics from "../components/Cosmetics.vue";
 import About from "../components/About.vue";
 import Settings from "../components/Settings.vue";
 
 const routes = [
   { path: "/", component: FishChecklist },
+  { path: "/cosmetics", component: Cosmetics },
   { path: "/about", component: About },
   { path: "/settings", component: Settings },
 ];
