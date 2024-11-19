8630082814f2de04cab304ce1bb045c1fa74bed1
Lucas <puksh@pm.me>
2024-11-19 10:48:22 +0100

    fix(App.vue): Removed not needed navigate method

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app-container">
-    <Sidebar @navigate="handleNavigation" />
+    <Sidebar />
     <main class="main-content">
       <router-view></router-view>
       <NotificationMessage />
