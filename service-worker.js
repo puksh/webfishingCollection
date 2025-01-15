a67d503d46d85d8ae76f04268997b13587d61dcb
puksh <puksh@users.noreply.github.com>
2025-01-15 09:05:24 +0000

    deploy: 60024550525287a2d280029b2fdba01c8ebe2abb

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "vue-pwa-cache";
+const urlsToCache = ["/", "/index.html", "/manifest.json", "/favicon.ico"];
+self.addEventListener("install", (event) => {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			return cache.addAll(urlsToCache);
+		}),
+	);
+});
+self.addEventListener("activate", (event) => {
+	const cacheWhitelist = [CACHE_NAME];
+	event.waitUntil(
+		caches.keys().then((cacheNames) => {
+			return Promise.all(
+				cacheNames.map((cacheName) => {
+					if (!cacheWhitelist.includes(cacheName)) {
+						return caches.delete(cacheName);
+					}
+				}),
+			);
+		}),
+	);
+});
+self.addEventListener("fetch", (event) => {
+	event.respondWith(
+		caches.match(event.request).then((cachedResponse) => {
+			if (cachedResponse) {
+				return cachedResponse;
+			}
+			return fetch(event.request);
+		}),
+	);
+});
