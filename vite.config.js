0d7c0002bc2acfa910cf949d9733ba0470a7848f
Lucas <puksh@pm.me>
2025-01-15 09:58:44 +0100

    feat: UPDATE TO VITE 6

--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,24 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: "autoUpdate" })],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  build: {
-    minify: "esbuild",
-    target: "esnext",
-    chunkSizeWarningLimit: 600,
-  },
-  css: {
-    preprocessorOptions: {
-      css: {
-        additionalData: `@import "./assets/main.css";`,
-      },
-    },
-  },
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+	build: {
+		minify: "esbuild",
+		target: "esnext",
+		chunkSizeWarningLimit: 600,
+	},
+	css: {
+		preprocessorOptions: {
+			css: {
+				additionalData: `@import "./assets/main.css";`,
+			},
+		},
+	},
 });
